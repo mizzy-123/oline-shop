44fb6478361760d5a22a533fd73cdba67e6391a6
colondum <decarabian21@gmail.com>
2023-09-24 16:06:52 +0700

    update

--- resources/views/main/pages/cart.blade.php
+++ resources/views/main/pages/cart.blade.php
@@ -30,16 +30,16 @@
       </div>
 
       <div class="row my-5">
-        {{-- <div class="col-lg-6 col-sm-6">
-          <div class="coupon-box">
+        <div class="col-lg-6 col-sm-6">
+          {{-- <div class="coupon-box">
             <div class="input-group input-group-sm">
               <input class="form-control" placeholder="Enter your coupon code" aria-label="Coupon code" type="text" />
               <div class="input-group-append">
                 <button class="btn btn-theme" type="button">Apply Coupon</button>
               </div>
             </div>
-          </div>
-        </div> --}}
+          </div> --}}
+        </div>
         <div class="col-lg-6 col-sm-6">
           <div class="update-box">
             <input value="Update Cart" type="submit" />
